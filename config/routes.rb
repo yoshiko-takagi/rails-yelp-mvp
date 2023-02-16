599836a8c6a09a2a8147047b51c413b87ac1abbe
Yoshiko Takagi <yoshiko.takagi49@gmail.com>
2023-02-16 14:35:42 +0900

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # HTTP verb 'path' to: 'controllername#action' as: prefix
-  # Read all
-  root to: 'restaurants#index'
-  get '/restaurants', to: 'restaurants#index', as: :restaurants
-  # Create_newRestaurant
-  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post '/restaurants', to: 'restaurants#create'
-
-  # Read one
-  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
-
-  # A visitor can add a new review to a restaurant
-  resources :restaurants do
+  resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
-
 end
